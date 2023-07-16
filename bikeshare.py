98fb1941e26970400ce428646476fb7fee60763d
Janny Pham <hientrangit94@gmail.com>
2023-07-16 19:36:25 +0800

    Create bikeshare.py to display the most common start hour

---
+++ bikeshare.py
@@ -0,0 +1,111 @@
+import pandas as pd
+import time
+
+CITY_DATA = {
+    'chicago': 'chicago.csv',
+    'new york city': 'new_york_city.csv',
+    'washington': 'washington.csv'
+}
+
+setMonth = [     
+    'all', 
+    'january', 
+    'february', 
+    'march', 
+    'april', 
+    'may', 
+    'june'
+]
+setDay = [
+    'all', 
+    'monday', 
+    'tuesday', 
+    'wednesday', 
+    'thursday', 
+    'friday', 
+    'saturday', 
+    'sunday'
+]
+
+def get_filters():
+    print('Hello! Let\'s explore some US bikeshare data!')
+    # Get user input for city (chicago, new york city, washington)
+    city = input("Would you like to see data for Chicago, New York City, or Washington? ").lower()
+    while city not in CITY_DATA:
+        print("Invalid city name. Please try again.")
+        city = input("Would you like to see data for Chicago, New York City, or Washington? ").lower()
+    
+    # Get user input for month (all, january, february, ... , june)
+    month = input("Which month? (all, january, february, ... , june) ").lower()
+    while month not in setMonth:
+        print("Invalid month. Please try again.")
+        month = input("Which month? (all, january, february, ... , june) ").lower()
+    
+    # Get user input for day of week (all, monday, tuesday, ... sunday)
+    day = input("Which day? (all, monday, tuesday, ... sunday) ").lower()
+    
+    while day not in setDay:
+        print("Invalid day. Please try again.")
+        day = input("Which day? (all, monday, tuesday, ... sunday) ").lower()
+    
+    print('-'*40)
+    return city, month, day
+
+def time_stats(df):
+    print('\nCalculating The Most Frequent Times of Travel...\n')
+    start_time = time.time()
+    
+    # Display the most common month
+    common_month = df['Month'].mode()[0]
+    print('Most Common Month:', common_month)
+    
+    # Display the most common day of week
+    common_day = df['Day of Week'].mode()[0]
+    print('Most Common Day of Week:', common_day)
+    
+    # Display the most common start hour
+    common_hour = df['Hour'].mode()[0]
+    print('Most Common Start Hour:', common_hour)
+    
+    print("\nThis took %s seconds." % (time.time() - start_time))
+    print('-'*40)
+
+
+def main():
+    while True:
+        city, month, day = get_filters()
+
+        df = pd.read_csv(CITY_DATA[city])
+        
+        df['Start Time'] = pd.to_datetime(df['Start Time'])
+        
+        df['Month'] = df['Start Time'].dt.month
+        df['Day of Week'] = df['Start Time'].dt.day_name()
+        df['Hour'] = df['Start Time'].dt.hour
+        
+        if month != 'all':
+            months = ['january', 'february', 'march', 'april', 'may', 'june']
+            month = months.index(month) + 1
+            df = df[df['Month'] == month]
+        
+        if day != 'all':
+            df = df[df['Day of Week'] == day.title()]
+        
+        time_stats(df)
+        
+        
+        start_row = 0
+        end_row = 5
+        raw_data = input("Would you like to see the raw data? (yes/no) ").lower()
+        while raw_data == 'yes':
+            print(df.iloc[start_row:end_row])
+            start_row += 5
+            end_row += 5
+            raw_data = input("Would you like to see more raw data? (yes/no) ").lower()
+        
+        restart = input('\nWould you like to restart? (yes/no) ')
+        if restart.lower() != 'yes':
+            break
+
+if __name__ == "__main__":
+    main()
